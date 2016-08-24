8baed9c20bb4f8c4011adf7453add0d9d846ba79
767327970@qq.com <767327970@qq.com>
2016-08-24 19:22:29 +0800

    update

--- nowcoder/PrintMinNumber.java
+++ nowcoder/PrintMinNumber.java
@@ -34,7 +34,7 @@ public class PrintMinNumber {
 		 Arrays.sort(nums, new Comparator<String>() {
 			@Override
 			public int compare(String s1, String s2) {
-				return (s1+s1).compareTo(s2+s1); //比较函数
+				return (s1+s1).compareTo(s2+s1); //比较函数，注意连接字符串后再比较
 			}
 		});
 		 String result=nums[0];
